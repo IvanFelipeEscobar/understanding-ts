686e52bad16e35852a62a248fe6360cee6dff40a
Ivan Escobar <ivnescbr1989@gmail.com>
2023-07-02 08:51:37 -0400

    geocoding request

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,19 @@ import axios from 'axios';
 const form = document.getElementById(`form-control`)!;
 const addressInput = document.getElementById(`address`)! as HTMLInputElement
 const apiKey = `AIzaSyBUvfeSMuzkgdoFcMJHI7CjlXvAYBx7xhA`
+type geoCodingResponse = {
+  results:{geometry:{location: {lat: number, lng: number}}}[];
+  status: `OK` | `ZERO_RESULTS`
+}
 const searchAdress = async (e: Event) => {
   e.preventDefault();
     const enteredAddress = addressInput.value
     try {
-    const httpsreq = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${apiKey}`);
-    console.log(httpsreq)
+    const httpsreq = await axios.get<geoCodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${apiKey}`);
+    
+    const coordinates = httpsreq.data.results[0].geometry.location
+    console.log(coordinates)
+    
     } catch (error) {
         console.log(error)
     }
